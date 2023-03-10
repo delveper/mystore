b22293516e2c900965ecff0877be8b21afc6ceee
delveper <yevhen@bilyk.dev>
2023-03-10 18:24:33 +0200

    feat: added variations

--- cmd/str/main.go
+++ cmd/str/main.go
@@ -3,25 +3,60 @@ package main
 import "strings"
 
 func concat(str []string) string {
-	var result string // smells less than result := ""
+	result := ""
 
-	for _, v := range str {
-		result += v // new string allocating on the heap
+	for _, v := range str { // loop over a copy
+		result += v //  new string allocation
 	}
 
 	return result
 }
 
-func concatNu(str []string) string {
-	return strings.Join(str, "")
-}
-
-func concatNuNu(str []string) string {
+func concatStringsBuilder(str []string) string {
 	var b strings.Builder
 
-	for _, s := range str {
-		b.WriteString(s)
+	var n int
+	for i := range str {
+		n += len(str[i])
+	}
+
+	b.Grow(n)
+
+	for i := range str {
+		b.WriteString(str[i])
 	}
 
 	return b.String()
+}
+
+func concatByteArr(str []string) string {
+	var n int
+	for i := range str {
+		n += len(str[i])
+	}
+
+	arr := make([]byte, 0, n)
+
+	n = 0
+	for i := range str {
+		arr = append(arr, str[i]...)
+	}
+
+	return string(arr)
+}
+
+func concatByteArrCopy(str []string) string {
+	var n int
+	for i := range str {
+		n += len(str[i])
+	}
+
+	arr := make([]byte, n)
+
+	n = 0
+	for i := range str {
+		n += copy(arr[n:], str[i])
+	}
+
+	return string(arr)
 }
--- cmd/str/main_test.go
+++ cmd/str/main_test.go
@@ -1,25 +1,36 @@
 package main
 
-import "testing"
+import (
+	_ "embed"
+	"strings"
+	"testing"
+)
 
-var testStrings = []string{
-	"foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz",
-}
+//go:embed testdata/data
+var testArr []byte
+var testStr = strings.Fields(string(testArr))
 
 func BenchmarkConcat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		concat(testStrings)
+		concat(testStr)
+	}
+
+}
+
+func BenchmarkConcatStringsBuilder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		concatStringsBuilder(testStr)
 	}
 }
 
-func BenchmarkConcatNu(b *testing.B) {
+func BenchmarkByteArr(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		concatNu(testStrings)
+		concatByteArr(testStr)
 	}
 }
 
-func BenchmarkConcatNuNu(b *testing.B) {
+func BenchmarkByteArrCopy(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		concatNuNu(testStrings)
+		concatByteArrCopy(testStr)
 	}
 }
